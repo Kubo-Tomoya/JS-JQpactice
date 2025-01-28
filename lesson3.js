f95d55ac3e97f00bdabe6a58b2347214386f08b3
久保友哉 <t.kubo.cingroup2012@gmail.com>
2025-01-28 22:00:48 +0900

    lesson1~5のファイルを削除

--- lesson3.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  // ボタンをクリックしたときにアニメーションを開始
-  $("#button").click(function () {
-      $(this).animate({
-          top: "+=300px", // 下に300px移動
-          left: "+=300px" // 右に300px移動
-      }, 1000); // 1秒でアニメーション完了
-  });
-});
